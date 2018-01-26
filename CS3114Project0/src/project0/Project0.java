85e71ba816ab69940711e0f35d62aeccb389b243
lancemiller <lancemiller@172.29.107.98>
2018-01-26 14:45:45 -0500

    Project whatup

---
+++ CS3114Project0/src/project0/Project0.java
@@ -0,0 +1,12 @@
+package project0;
+
+import java.util.*;
+import java.io.*;
+//Add imports as needed
+
+public class Project0 {	
+	public static void main(String[] args) {
+		//Insert your code here
+		
+	}
+}
