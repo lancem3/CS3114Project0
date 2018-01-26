2363ddf0d1c22bf3ab38e650b82a6fbc33d9f4dd
DTM1015 <DTM1015@DylanPC>
2018-01-26 16:10:54 -0500

    Testing

--- CS3114Project0/src/project0/Project0.java
+++ CS3114Project0/src/project0/Project0.java
@@ -2,6 +2,7 @@ package project0;
 
 import java.util.*;
 import java.io.*;
+import java.util.ArrayList;
 //Add imports as needed
 
 public class Project0 {	
@@ -29,4 +30,43 @@ public class Project0 {
 		    System.out.println("");
 		}
 	}
-}
+	
+	private static class AdjacencyList {
+	    private ArrayList<Integer>[] edges;
+	    /**
+	     * 
+	     * @param length
+	     */
+	    public AdjacencyList(int length){
+	        edges = new ArrayList[length];
+	        for(int i = 0; i < edges.length; i++){
+	            edges[i] = new ArrayList<Integer>();
+	        }
+	    }
+	    /**
+	     * 
+	     * @param vertex1
+	     * @param vertex2
+	     * @return
+	     */
+	    public boolean addEdge(int vertex1, int vertex2){
+	        if(edges[vertex1].contains(vertex2) || edges[vertex2].contains(vertex1)){
+	            return false;
+	        }
+	        edges[vertex1].add(vertex2);
+	        edges[vertex2].add(vertex1);
+	        return true;
+	    }
+	    /**
+	     * 
+	     * @return
+	     */
+	    public int[] getAdjacentVertices(int vertex){  
+	        int[] vertices = new int[edges[vertex].size()];
+	        for(int i = 0; i < vertices.length; i++){
+	            vertices[i] = edges[vertex].get(i);
+	        }
+	        return vertices;
+	    }
+	}
+	}
