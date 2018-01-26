9bb6a3a47a95602e055f2fafc1114a1565570f9f
DTM1015 <DTM1015@DylanPC>
2018-01-26 15:24:53 -0500

    Basic AdjList funcitonality

---
+++ CS3114Project0/src/project0/AdjacencyList.java
@@ -0,0 +1,43 @@
+package project0;
+
+import java.util.ArrayList;
+
+/**
+ * @author Dylan Michaels(dtm1015), Lance Miller(lancem3)
+ * @version Jan 26, 2018
+ */
+public class AdjacencyList {
+    private ArrayList<Integer>[] edges;
+    /**
+     * 
+     * @param length
+     */
+    public AdjacencyList(int length){
+        edges = new ArrayList[length];
+    }
+    /**
+     * 
+     * @param vertex1
+     * @param vertex2
+     * @return
+     */
+    public boolean addEdge(int vertex1, int vertex2){
+        if(edges[vertex1].contains(vertex2) ||edges[vertex2].contains(vertex1)){
+            return false;
+        }
+        edges[vertex1].add(vertex2);
+        edges[vertex2].add(vertex1);
+        return true;
+    }
+    /**
+     * 
+     * @return
+     */
+    public int[] getAdjacentVertices(int vertex){  
+        int[] vertices = new int[edges[vertex].size()];
+        for(int i = 0; i < vertices.length; i++){
+            vertices[i] = edges[vertex].get(i);
+        }
+        return vertices;
+    }
+}
